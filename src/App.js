a42bfda03ac71e0dec5422e03a9b6eb2b64ab455
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-24 14:24:21 +0530

    header with form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Values from "values.js";
 import SingleColor from './SingleColor';
 
 import "./styles.css";
 
 export default function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
   return (
     <div className="App">
-      <h2>color generator app</h2>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+            value={color}
+            placeholder="#f15025" 
+            onChange={(e) => setColor(e.target.value)} />
+          <button type="submit">Generate</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List goes here</h4>
+      </section>
     </div>
   );
 }
