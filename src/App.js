a197db2814129e4f599c89c00626a6a8e8292c2c
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-24 16:10:50 +0530

    generated color

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 import React, { useState } from "react";
 import Values from "values.js";
-import SingleColor from './SingleColor';
+import SingleColor from "./SingleColor";
 
 import "./styles.css";
 
 export default function App() {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState("");
   const [error, setError] = useState(false);
   const [list, setList] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      let colors = new Values(color).all(20);
-      console.log(colors);
+      let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
       setError(true);
       console.log(error);
     }
-  }
+  };
   return (
     <div className="App">
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" 
+          <input
+            type="text"
             value={color}
-            placeholder="#f15025" 
-            className={ `${ error ? 'error' : null }` }
-            onChange={(e) => setColor(e.target.value)} />
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+            onChange={(e) => setColor(e.target.value)}
+          />
           <button type="submit">Generate</button>
         </form>
       </section>
       <section className="colors">
-        <h4>List goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </div>
   );
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import rgbToHex from './utils';
 
-const SingleColor = () => {
+const SingleColor = ({rgb, weight, index, hexColor}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
   return (
-    <h1>Hello</h1>
+    <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColor}</p>
+    </article>
   );
 }
 
