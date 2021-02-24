499bcfa41fbac60b53251938bf14d8115be0fc51
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-24 13:59:19 +0530

    added values.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Values from "values.js";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>color generator app</h2>
     </div>
   );
 }
